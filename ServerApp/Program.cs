a19305de3388eb51834de27d6289d467e85d3273
Алешков Максим Романович <Alesh_rg62@itstep.edu.az>
2023-11-02 21:48:08 +0400

    created endpoint in server

--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+string serverString = "127.0.0.1:8080";
+
+IPEndPoint.TryParse(serverString, out IPEndPoint? endPoint);
+
+if(endPoint is null)
+    return;
