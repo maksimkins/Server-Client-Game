f87a29080eab05f3b94bf0c7d639da540e3654fe
maksimkins <maks.aleshkov.04@gmail.com>
2023-11-03 19:52:22 +0400

    add client and server with only one client

--- ClientApp/Program.cs
+++ ClientApp/Program.cs
@@ -1,4 +1,44 @@
-﻿using System.Net;
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System;
+
+string serverstr = "127.0.0.1";
+int port = 8080;
+
+IPAddress.TryParse(serverstr, out IPAddress? ipAdress);
+
+if (ipAdress is null)
+    return;
+
+IPEndPoint endPoint = new IPEndPoint(
+    ipAdress, port
+    );
+
+Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+serverSocket.Connect(endPoint);
+
+byte[] buffer = new byte[1024];
+serverSocket.Receive(buffer);
+Console.WriteLine(Encoding.ASCII.GetString(buffer));
 
 
-EndPoint endPoint;
+for (int i = 0; i < 5; i++)
+{
+    //if (!serverSocket.Connected)
+    //{
+    //    break;
+    //}
+
+    Array.Clear(buffer);
+    Console.Write("input num: ");
+    string num = Console.ReadLine() ?? "";
+
+    serverSocket.Send(Encoding.ASCII.GetBytes(num));
+    serverSocket.Receive(buffer);
+    Console.WriteLine(Encoding.ASCII.GetString(buffer));
+
+}
+
+
--- ServerApp/Program.cs
+++ ServerApp/Program.cs
@@ -1,8 +1,52 @@
 ﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-string serverString = "127.0.0.1:8080";
+string serverstr = "127.0.0.1";
+int port = 8080;
 
-IPEndPoint.TryParse(serverString, out IPEndPoint? endPoint);
+IPAddress.TryParse(serverstr, out IPAddress? ipAdress);
 
-if(endPoint is null)
+if(ipAdress is null)
     return;
+
+IPEndPoint endPoint = new IPEndPoint(
+    ipAdress, port
+    );
+
+using Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+serverSocket.Bind(endPoint);
+serverSocket.Listen();
+
+using Socket clientSocket = serverSocket.Accept();
+
+clientSocket.Send(Encoding.ASCII.GetBytes("(num is in [0; 100])"));
+byte[] buffer = new byte[1024];
+
+int num = Random.Shared.Next(0, 100);
+Console.WriteLine($"{clientSocket}: num = {num}");
+
+for (int i = 0; i < 5; i++)
+{
+    clientSocket.Receive(buffer);
+    string message = Encoding.ASCII.GetString(buffer);
+
+    if (int.TryParse(message, out int result) && result == num)
+    {
+        buffer = new byte[1024];
+        clientSocket.Send(Encoding.ASCII.GetBytes($"you win"));
+        clientSocket.Close();
+        clientSocket.Disconnect(false);
+        break;
+    }
+    else
+    {
+        clientSocket.Send(Encoding.ASCII.GetBytes($"you have {4 - i} trial(s)"));
+        continue;
+    }
+    
+
+}
+
+
